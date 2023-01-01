f653816a701f481853ef93b2b21e20d9b74904fd
yeal911 - Ubuntu/Matebook X Pro <yeal911@qq.com>
2023-01-01 15:39:02 +0100

    updated UI, add displaying note

--- app/src/main/java/com/taoping/ir_piano/MainActivity.java
+++ app/src/main/java/com/taoping/ir_piano/MainActivity.java
@@ -1,90 +1,71 @@
 package com.taoping.ir_piano;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewAnimator;
 
-import org.w3c.dom.Text;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private PianoKeyboardView keyboard;
     private TextView noteText;
 
-    @SuppressLint("ClickableViewAccessibility")
+    @SuppressLint({"ClickableViewAccessibility", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        final Switch transmissionSwitch = findViewById(R.id.switchWifiIR);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        @SuppressLint("UseSwitchCompatOrMaterialCode") final Switch transmissionSwitch = findViewById(R.id.switchWifiIR);
         final Button searchIPBtn = (Button) findViewById(R.id.searchReceiverBtn);
         final Button switchModeBtn = (Button) findViewById(R.id.switchBtn);
         final EditText ipText = (EditText) findViewById(R.id.ipText);
         keyboard = (PianoKeyboardView) findViewById(R.id.piano_keyboard_view);
         noteText = (TextView) findViewById(R.id.noteText);
 
-        transmissionSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(transmissionSwitch.isChecked()){
-                    transmissionSwitch.setText("   IR");
-                    ipText.setVisibility(View.INVISIBLE);
-                    searchIPBtn.setVisibility(View.INVISIBLE);
-                }else{
-                    transmissionSwitch.setText("Wifi");
-                    ipText.setVisibility(View.VISIBLE);
-                    searchIPBtn.setVisibility(View.VISIBLE);
-                }
+        transmissionSwitch.setOnClickListener(view -> {
+            if(transmissionSwitch.isChecked()){
+                transmissionSwitch.setText("   IR");
+                ipText.setVisibility(View.INVISIBLE);
+                searchIPBtn.setVisibility(View.INVISIBLE);
+            }else{
+                transmissionSwitch.setText("Wifi");
+                ipText.setVisibility(View.VISIBLE);
+                searchIPBtn.setVisibility(View.VISIBLE);
             }
         });
 
-        switchModeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchMode();
-                // Perform action on click
-                Toast.makeText(MainActivity.this, "btn is clicked!", Toast.LENGTH_SHORT).show();
-            }
+        switchModeBtn.setOnClickListener(v -> {
+            switchMode();
+            // Perform action on click
+            Toast.makeText(MainActivity.this, "btn is clicked!", Toast.LENGTH_SHORT).show();
         });
 
-        searchIPBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                searchReceiverIP();
-                // Perform action on click
-                Toast.makeText(MainActivity.this, "btn is clicked!", Toast.LENGTH_SHORT).show();
-            }
+        searchIPBtn.setOnClickListener(v -> {
+            searchReceiverIP();
+            // Perform action on click
+            Toast.makeText(MainActivity.this, "btn is clicked!", Toast.LENGTH_SHORT).show();
         });
 
-        keyboard.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                keyboard.pianoOnTouch(motionEvent);
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN && keyboard.pressedKey != -1)
-                    notePressDown();
-                else if (motionEvent.getAction() == MotionEvent.ACTION_UP)
-                    notePressUp();
-                return true;
-            }
+        keyboard.setOnTouchListener((view, motionEvent) -> {
+            keyboard.pianoOnTouch(motionEvent);
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN && keyboard.pressedKey != -1)
+                notePressDown();
+            else if (motionEvent.getAction() == MotionEvent.ACTION_UP)
+                notePressUp();
+            return true;
         });
 
 
@@ -101,9 +82,9 @@ public class MainActivity extends AppCompatActivity {
     private void notePressDown(){
         ViewGroup.LayoutParams noteTextParam = noteText.getLayoutParams();
         if(keyboard.isWhiteKey(keyboard.pressedKey))
-            noteTextParam.width = keyboard.WHITE_KEY_WIDTH;
+            noteTextParam.width = PianoKeyboardView.WHITE_KEY_WIDTH;
         else
-            noteTextParam.width = keyboard.BLACK_KEY_WIDTH;
+            noteTextParam.width = PianoKeyboardView.BLACK_KEY_WIDTH;
         noteText.setLayoutParams(noteTextParam);
         ViewAnimator animator = findViewById(R.id.animator);
         animator.clearAnimation();
